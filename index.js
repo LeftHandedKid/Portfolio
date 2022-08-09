8898ba7755f35303b89abbc89913d9960ae11583
Isaiah <trickmiester21@gmail.com>
2022-08-09 02:35:03 -0500

    added working navicon

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// declaration of variables
 const date = document.querySelector("#date");
 const exploreButton = document.querySelector("#explore");
 const navbar = document.querySelector(".navbar");
+const navicon = document.querySelector("#navicon")
 
 // update portfolio with current year (year located in footer)
 currentYear = new Date().getFullYear();
@@ -9,5 +11,10 @@ date.textContent = currentYear;
 // have hero image scroll smoothly to first header
 exploreButton.addEventListener("click", () => {
     navbar.scrollIntoView({ behavior: "smooth", block: "start" });
+});
+
+// have navicon hold its position (on or off)
+navicon.addEventListener("click", () => {
+    navicon.classList.toggle("active");
 });
 

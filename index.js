92b8b2a8681fef5183aa946abdc4c64724956a2e
Isaiah <trickmiester21@gmail.com>
2022-08-09 03:13:21 -0500

    add incomplete menu and change navicon query css

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// declaration of variables
+// declaration/initialization of variables
 const date = document.querySelector("#date");
 const exploreButton = document.querySelector("#explore");
 const navbar = document.querySelector(".navbar");
 const navicon = document.querySelector("#navicon")
+const menu = document.createElement("menu");
 
 // update portfolio with current year (year located in footer)
 currentYear = new Date().getFullYear();
@@ -16,5 +17,14 @@ exploreButton.addEventListener("click", () => {
 // have navicon hold its position (on or off)
 navicon.addEventListener("click", () => {
     navicon.classList.toggle("active");
+
+    // show the menu when navicon is on
+    if (navicon.classList.contains("active")) {
+        menu.classList.add("menu");
+        navbar.appendChild(menu);
+    } else {
+        navbar.removeChild(menu);
+    };
 });
+
 
